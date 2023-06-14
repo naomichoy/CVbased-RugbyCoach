f2d2d110ee0492c608bc30474072ba88db8cdd29
naomichoy <choyn08@connect.hku.hk>
2023-06-14 01:14:04 +0100

    init

---
+++ extract_frame.py
@@ -0,0 +1,36 @@
+import cv2
+from PIL import Image
+
+def extract_frames(video_path, output_path):
+    # Open the video file
+    video = cv2.VideoCapture(video_path)
+    success, frame = video.read()
+    count = 0
+
+    while success:
+        color_coverted = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Convert the frame to PIL Image format
+        pil_image = Image.fromarray(color_coverted)
+
+        # Save the frame as an image file
+        frame_path = f"{output_path}/frame_{str(count).zfill(12)}.jpg"
+        # cv2.imwrite(frame_path, frame)
+        pil_image.save(frame_path)
+
+        # Read the next frame
+        success, frame = video.read()
+        count += 1
+
+    # Release the video file
+    video.release()
+
+video_name = "s2"
+
+# Provide the path to the video file
+video_path = f"examples/media/{video_name}.mp4"
+
+# Provide the path to the output directory where the frames will be saved
+output_path = f"frames/{video_name}"
+
+# Call the function to extract frames
+extract_frames(video_path, output_path)
---
+++ map_keypoints.py
@@ -0,0 +1,179 @@
+import os
+import re
+import json
+import cv2
+from PIL import Image
+import numpy as np
+
+def read_json_file(file_path):
+    with open(file_path) as file:
+        data = json.load(file)
+    return data
+
+# def create_mask(image_shape, polygon):
+#     mask = np.zeros(image_shape[:2], dtype=np.uint8)
+#     cv2.polylines(mask, [polygon], isClosed=True, color=(255, 255, 255), thickness=2)
+#     cv2.fillPoly(mask, [polygon], color=(255, 255, 255))
+#     return mask
+
+def is_above_line(point, line):
+    x1, y1 = line[0]
+    x2, y2 = line[1]
+    x, y = point
+    offset = 3
+
+    # Calculate the y-coordinate of the line at the given x-coordinate
+    line_y = ((y2 - y1) / (x2 - x1)) * (x - x1) + y1
+    # print(y, line_y)
+
+    # Compare the y-coordinate of the point to the y-coordinate of the line
+    if y + offset < line_y:
+        return True
+    else:
+        return False
+
+
+def is_left_of_line(point, line):
+    x1, y1 = line[0]
+    x2, y2 = line[1]
+    x, y = point
+
+    # Calculate the cross product of vectors (line vector) x (point vector)
+    cross_product = (x2 - x1) * (y - y1) - (y2 - y1) * (x - x1)
+
+    # Check if the cross product is positive (point to the left of the line)
+    if cross_product > 0:
+        return True
+    else:
+        return False
+
+
+def is_right_of_line(point, line):
+    x1, y1 = line[0]
+    x2, y2 = line[1]
+    x, y = point
+
+    # Calculate the cross product of vectors (line vector) x (point vector)
+    cross_product = (x2 - x1) * (y - y1) - (y2 - y1) * (x - x1)
+
+    # Check if the cross product is positive (point to the right of the line)
+    if cross_product < 0:
+        return True
+    else:
+        return False
+
+
+video_name = "s2"
+folder_path = f"output/{video_name}"
+
+# # define detection area
+# five_meter = [[965, 610], [965, 715], [1860, 685], [1580, 615]]
+# five_meter = np.array(five_meter)
+# five_meter = five_meter.reshape((-1, 1, 2))
+#
+# ten_meter = [[295, 610], [165, 715], [965, 610], [965, 715]]
+# ten_meter = np.array(ten_meter)
+# ten_meter = ten_meter.reshape((-1, 1, 2))
+#
+# # create area mask
+# five_meter_mask = create_mask(frame.shape, five_meter)
+# cv2.imshow("5 meter Mask", five_meter_mask)
+# cv2.waitKey(0)
+
+# define lines
+gnd_line = [[1894, 644], [1667, 653]]
+five_meter_line = [[960, 330], [960, 805]]
+ten_meter_line = [[195, 330], [195, 805]]
+direction = "left"  # direction towards
+
+# init frame counters
+gnd_trigger = False
+five_meter_trigger = False
+five_meter_counter = 0
+ten_meter_counter = 0
+
+
+for filename in os.listdir(folder_path):
+    # Check if the path is a file
+    if os.path.isfile(os.path.join(folder_path, filename)):
+        # read JSON file
+        numbers = re.findall(r'_(\d+)_', filename)
+        frame_number = ''.join(numbers)
+        # filled_number = str(frame_number).zfill(12)
+        # json_file_path = f"output/{video_name}/{video_name}_{filled_number}_keypoints.json"
+        # json_data = read_json_file(json_file_path)
+        json_data = read_json_file(os.path.join(folder_path, filename))
+        try:
+            keypoints = json_data['people'][0]['pose_keypoints_2d']
+        except IndexError:
+            # print(json_data)
+            continue
+
+        # JSON read check
+        # print(len(keypoints))   # 75 -> 25 keypoints x3, x, y, conf
+
+        # read frame
+        frame_path = f"frames/s2/frame_{frame_number}.jpg"
+        frame = cv2.imread(frame_path)
+        # print(frame.shape)
+
+        # extract keypoints to keypoints_dict for easier interpretation
+        keypoints_dict = [{0: ""}]
+        for i in range(0, len(keypoints), 3):
+            # extract keypoints
+            keypoint_num = int(i/3)
+            point_to_draw = (round(keypoints[i]), round(keypoints[i+1]))
+            # print(keypoint_num, point_to_draw)
+            keypoints_dict[0][keypoint_num] = point_to_draw
+
+            # draw on points on frame
+            cv2.circle(frame, point_to_draw, 2, (0, 255, 0), 2)
+
+        # print(keypoints_dict)
+
+        if not gnd_trigger and not five_meter_trigger:
+            if is_above_line(keypoints_dict[0][19], gnd_line):
+                print("LBigToe off ground", frame_number)
+                gnd_trigger = True
+            elif is_above_line(keypoints_dict[0][22], gnd_line):
+                print("RBigToe off ground", frame_number)
+                gnd_trigger = True
+
+        if direction == "left" and gnd_trigger:
+            ten_meter_counter += 1
+            if not five_meter_trigger:
+                five_meter_counter += 1
+            if is_left_of_line(keypoints_dict[0][8], five_meter_line) and not five_meter_trigger:
+                print("five meters", five_meter_counter)
+                five_meter_trigger = True
+            elif is_left_of_line(keypoints_dict[0][8], ten_meter_line):
+                print("ten meters", ten_meter_counter)
+                gnd_trigger = False
+
+        elif direction == "right" and gnd_trigger:
+            ten_meter_counter += 1
+            if not five_meter_trigger:
+                five_meter_counter += 1
+            if is_right_of_line(keypoints_dict[0][8], five_meter_line) and not five_meter_trigger:
+                print("five meters", five_meter_counter)
+                five_meter_trigger = True
+            elif is_right_of_line(keypoints_dict[0][8], ten_meter_line):
+                print("ten meters", ten_meter_counter)
+                gnd_trigger = False
+
+        # # draw area on frame
+        # cv2.polylines(frame, [five_meter], isClosed=True, color=(0, 255, 0), thickness=2)
+
+        # draw line on frame
+        cv2.line(frame, gnd_line[0], gnd_line[1], (0, 0, 255), 2)
+        cv2.line(frame, five_meter_line[0], five_meter_line[1], (0, 0, 255), 2)
+        cv2.line(frame, ten_meter_line[0], ten_meter_line[1], (0, 0, 255), 2)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# cv2.imshow(f"{frame_number}", frame)
+# cv2.waitKey(0)
+cv2.destroyAllWindows()
+
